5b04c0a19c5190a9bc1e18e7ba955a97c3d46d4b
luojiebin <luo.jiebin@qq.com>
2017-10-10 06:11:44 -0700

    finished hw1-q5

--- hw1/hw1-q5-while.sql
+++ hw1/hw1-q5-while.sql
@@ -3,16 +3,41 @@
 DROP TABLE IF EXISTS q5_extended_paths;
 CREATE TABLE q5_extended_paths(src, dest, length, path)
 AS
-    SELECT -- remember to remove duplicates!
+  SELECT p.src, e.dest, p.length + e.length, ARRAY_APPEND(p.path, e.dest)
+  FROM q5_paths_to_update AS p, q5_edges AS e
+  WHERE p.dest = e.src AND NOT p.path @> ARRAY[e.dest];
 ;
 
 CREATE TABLE q5_new_paths(src, dest, length, path)
 AS
-   SELECT -- fill this in
+  SELECT *
+  FROM q5_extended_paths AS extended
+  WHERE NOT EXISTS
+              (SELECT *
+               FROM q5_paths AS paths
+               WHERE paths.src = extended.src
+               AND paths.dest = extended.dest)
+  UNION
+  SELECT extended.*
+  FROM q5_extended_paths AS extended, q5_paths AS paths
+  WHERE extended.src = paths.src
+  AND extended.dest = paths.dest
+  AND extended.length < paths.length
+;
 
 CREATE TABLE q5_better_paths(src, dest, length, path)
 AS 
-    SELECT -- fill this in
+    SELECT *
+    FROM q5_new_paths
+    UNION
+    SELECT *
+    FROM q5_paths
+    WHERE NOT EXISTS
+                (SELECT *
+                 FROM q5_new_paths
+                 WHERE q5_new_paths.src = q5_paths.src
+                 AND q5_new_paths.dest = q5_paths.dest)
+;
 
 DROP TABLE q5_paths;
 ALTER TABLE q5_better_paths RENAME TO q5_paths;
